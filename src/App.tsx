f358f5abd9e055b8814b8017a5220c413a2b9610
rhomaios <rodion70ch60@gmail.com>
2024-06-07 12:00:14 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,22 @@ import {Product, state} from "./data/state.ts";
 import {ProductCard} from "./components/ProductCard.tsx";
 import {Button} from "./components/Basic/Button.tsx";
 import {getChatId, sendMessage} from "./Http/telegram.ts";
-import React, {useEffect} from "react";
 
 function App() {
-    const [chat, setChat] = React.useState<any>(null);
-
-    useEffect(() => {
-        getChatId().then(data => setChat(data))
-    });
 
     async function handleCart(item: Product) {
-        sendMessage({
-            chat_id: chat.data.result[0].message.chat.id,
-            message: `Принят заказ на товар "${item.title}".\nЦена: ${item.price} руб.`
-        }).then(response => {
-            console.log('Message sent successfully:', response.data);
-        }).catch(error => {
-            console.error('Error sending message:', error);
-        });
+        getChatId().then(data => {
+            if(data.data.result.length > 0) {
+                sendMessage({
+                    chat_id: data.data.result[0].message.chat.id,
+                    message: `Принят заказ на товар "${item.title}".\nЦена: ${item.price} руб.`
+                }).then(response => {
+                    console.log('Message sent successfully:', response.data);
+                }).catch(error => {
+                    console.error('Error sending message:', error);
+                });
+            }
+        })
     }
 
   return (
