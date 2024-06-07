be7ce4fa96246eafd856098657a585ad2deef768
rhomaios <rodion70ch60@gmail.com>
2024-06-07 13:03:45 +0700

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import {Product, state} from "./data/state.ts";
 import {ProductCard} from "./components/ProductCard.tsx";
 import {Button} from "./components/Basic/Button.tsx";
-import {getUpdates, sendMessage} from "./Http/telegram.ts";
+import {sendMessage} from "./Http/telegram.ts";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
@@ -10,20 +10,16 @@ function App() {
 
     async function handleCart(item: Product) {
 
-        getUpdates().then(data => {
-            console.log(data);
-            console.log(user);
-            if(data.data.result.length > 0) {
-                sendMessage({
-                    chat_id: data.data.result[0].message.chat.id,
-                    message: `Принят заказ на товар "${item.title}".\nЦена: ${item.price} руб.`
-                }).then(response => {
-                    console.log('Message sent successfully:', response.data);
-                }).catch(error => {
-                    console.error('Error sending message:', error);
-                });
-            }
-        })
+        if (user) {
+            sendMessage({
+                chat_id: user.id,
+                message: `Принят заказ на товар "${item.title}".\nЦена: ${item.price} руб.`
+            }).then(response => {
+                console.log('Message sent successfully:', response.data);
+            }).catch(error => {
+                console.error('Error sending message:', error);
+            });
+        }
     }
 
   return (
